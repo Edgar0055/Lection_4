542eb3c0c1dacf71bf2dd939593e816faa3c8ccd
Edgar0055 <e.rostomian13@gmail.com>
2019-09-30 02:35:29 +0300

    fix final commit

--- app.js
+++ app.js
@@ -1,45 +1,46 @@
-const $http = require( 'http' );
-const $url = require( 'url' );
+const $http = require('http');
+const $url = require('url');
 
-$http.createServer( ( req, res ) => {
-    const url = $url.parse( req.url, true, );
-    const method = req.method;
-    if ( url.pathname === '/' && method === 'GET' ) {
-        res.writeHead( 200, { 'Content-Type': 'text/html' } );
-        const fieldName = url.query.fieldName || 'field';
+$http.createServer((req, res) => {
+    const { URL } = $url;
+    const { headers: { host }, method, url } = req;
+    const uri = new URL(`http://${host}${url}`);
+    if (method === 'GET' && uri.pathname === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        const fieldName = uri.searchParams.get('fieldName') || 'fieldName';
         // const enctype = 'multipart/form-data';
         const enctype = 'application/x-www-form-urlencoded';
         // const enctype = 'text/plain';
-        res.end( `
+        res.end(`
         <html>
             <body>
-                <form action="/" method="post" enctype="${ enctype }" >
-                    <input type="text" name="${ fieldName }" />
+                <form action="/" method="post" enctype="${enctype}" >
+                    <input type="text" name="${fieldName}" />
                     <input type="submit" value="send" />
                 </form> 
             </body> 
-        </html>` );        
-    } else if ( url.pathname === '/' && method === 'POST' ) {
+        </html>`);        
+    } else if (method === 'POST' && uri.pathname === '/') {
         let data = '';
-        req.on( 'data', ( chunk ) => {
+        req.on('data', (chunk) => {
             data += chunk;
-        } ).on( 'end', () => {
-            const { query } = $url.parse( `?${ data }`, true );
-            const [ [ name, value ] ] = [ ...Object.entries( query ) ];
-            res.writeHead( 200, { 'Content-Type': 'text/html' } );
-            res.end( `
+        }).on('end', () => {
+            const { query } = $url.parse(`?${data}`, true);
+            const [[name, value]] = [...Object.entries(query)];
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(`
             <html>
                 <body>
                     <h4>Result:</h4>
-                    <div><span><b>Name</b>: ${ name }</span><span><b>Value</b>: ${ value }</span></div>
+                    <div><span><b>Name</b>: ${name}</span><span><b>Value</b>: ${value}</span></div>
                 </body> 
-            </html>` );    
-        } ).on( 'error', () => {
-            res.writeHead( 403, { } );
-            res.end( );
-        } );
+            </html>`);    
+        }).on('error', () => {
+            res.writeHead(403, { });
+            res.end();
+        });
     } else {
-        res.writeHead( 404, { } );
-        res.end( );
+        res.writeHead(404, { });
+        res.end();
     }
-} ).listen( 4000 );
+}).listen(4000);
